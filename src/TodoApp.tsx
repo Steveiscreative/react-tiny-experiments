33bcbc0f2c089dd2055b80cc2052b568350cf0d7
Steve Stephenson <ttwstephenson@gmail.com>
2025-06-08 09:27:08 -0400

    Adds delete functionality. Shows indicator when savings/saved.

--- src/TodoApp.tsx
+++ src/TodoApp.tsx
@@ -9,7 +9,7 @@ interface Task {
 
 export default function TodoApp() {
   const [newText, setNewText] = useState("");
-
+  const [isSaving, setIsSaving] = useState(false);
   const [tasks, setTasks] = useState<Task[]>(() => {
     const saved = localStorage.getItem("my-todo-list");
     return saved ? JSON.parse(saved) : [];
@@ -36,12 +36,24 @@ export default function TodoApp() {
     setNewText("");
   }
 
+  function deleteTask(id: number) {
+    //filter
+    setTasks((ts) => ts.filter((t) => t.id !== id));
+  }
+
   useEffect(() => {
+    setIsSaving(true);
     localStorage.setItem("my-todo-list", JSON.stringify(tasks));
+    setTimeout(() => {
+      setIsSaving(false);
+    }, 500);
   }, [tasks]);
 
   return (
     <div className="Todo w-lg m-auto pt-5">
+      <div className="uppercase text-xs mb-1">
+        {isSaving ? "Saving" : "Saved"}
+      </div>
       <div className="flex justify-between bg-gray-100 p-3 mb-2">
         {" "}
         <input
@@ -60,21 +72,32 @@ export default function TodoApp() {
       </div>
       <ul>
         {tasks.map((task) => (
-          <li key={task.id} className="border-b border-gray-100 py-2">
-            <input
-              onChange={() => updateCompleteStatus(task.id)}
-              type="checkbox"
-              checked={task.completed}
-            />{" "}
-            <span
-              className={
-                task.completed
-                  ? "line-through decoration-red-900 decoration-4"
-                  : ""
-              }
+          <li
+            key={task.id}
+            className="border-b border-gray-100 py-2 flex justify-between"
+          >
+            <div>
+              <input
+                onChange={() => updateCompleteStatus(task.id)}
+                type="checkbox"
+                checked={task.completed}
+              />{" "}
+              <span
+                className={
+                  task.completed
+                    ? "line-through decoration-red-900 decoration-4"
+                    : ""
+                }
+              >
+                {task.text}
+              </span>
+            </div>
+            <button
+              onClick={() => deleteTask(task.id)}
+              className="text-sm text-red-500 cursor-pointer"
             >
-              {task.text}
-            </span>
+              Delete
+            </button>
           </li>
         ))}
       </ul>
