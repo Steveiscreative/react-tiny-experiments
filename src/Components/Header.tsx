0e07cfe66374f3441cb5e8a63e1214b9006985d9
Steve Stephenson <ttwstephenson@gmail.com>
2025-06-10 22:58:52 -0400

    Toggle alignment

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -6,7 +6,7 @@ import ThemeToggle from "./ThemeToggle";
 
 export default function SiteHeader() {
   return (
-    <header className="site-header fixed w-full">
+    <header className="site-header fixed w-90 bg-white/90 rounded-3xl inset-x-0 mx-auto top-3">
       <ThemeToggle />
     </header>
   );
